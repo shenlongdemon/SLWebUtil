1bd1cdf11d007478c6b8a998d3675c66aafcf5bb
shenlongdemon <shenlongdemon@gmail.com>
2015-10-31 01:24:31 +0700

    Add MedicineRepository project

---
+++ MedicineRepository/MedicineRepo.cs
@@ -0,0 +1,28 @@
+﻿using RepositoryUtility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicineRepository
+{
+    public class MedicineRepo
+    {
+        private static MedicineRepo _MedicineRepo;
+        private SLWebUtilContext dbContext = null;
+        public static MedicineRepo Instance()
+        {
+            if (_MedicineRepo == null)
+            {
+                _MedicineRepo = new MedicineRepo();
+                _MedicineRepo.dbContext = new SLWebUtilContext();
+            }
+            return _MedicineRepo;
+        }
+        public EFRepository<T> GetRepo<T>() where T : class
+        {            
+            return new EFRepository<T>(dbContext);
+        }
+    }
+}
